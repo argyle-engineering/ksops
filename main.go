f2b3a5ab9b04c779cd5158313815352a7bfd223e
Nathan M <nathan.eua@gmail.com>
2021-10-17 19:25:01 -0300

    Initial working implementation

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go.mozilla.org/sops/v3/cmd/sops/formats"
+	"go.mozilla.org/sops/v3/decrypt"
+	"io/ioutil"
+	"os"
+	"sigs.k8s.io/yaml"
+)
+
+type resource struct {
+	Files        []string `json:"files,omitempty" yaml:"files,omitempty"`
+	FailSilently bool     `json:"fail-silently,omitempty" yaml:"fail-silently,omitempty"`
+}
+
+func main() {
+
+	if len(os.Args) > 2 {
+		_, _ = fmt.Fprintf(os.Stderr, "always invoke this via kustomize plugins, received too many args: %d\n", len(os.Args))
+		os.Exit(1)
+	}
+
+	if len(os.Args) < 2 {
+		_, _ = fmt.Fprintf(os.Stderr, "always invoke this via kustomize plugins, received too few args: %d\n", len(os.Args))
+		os.Exit(1)
+	}
+
+	content, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to read file: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+
+	var manifest resource
+	err = yaml.Unmarshal(content, &manifest)
+
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error unmarshalling manifest content: %q \n%s\n", err, content)
+		os.Exit(1)
+	}
+
+	if manifest.FailSilently {
+		os.Exit(0)
+	}
+
+	if manifest.Files == nil {
+		_, _ = fmt.Fprintf(os.Stderr, "missing the required 'files' key in the ksops manifests: %s", content)
+		os.Exit(1)
+	}
+
+	var output bytes.Buffer
+
+	for _, file := range manifest.Files {
+
+		var b, data []byte
+
+		b, err = ioutil.ReadFile(file)
+
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "error reading %q: %q\n", file, err.Error())
+			os.Exit(1)
+		}
+
+		format := formats.FormatForPath(file)
+		data, err = decrypt.DataWithFormat(b, format)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "trouble decrypting file %s\n", err.Error())
+			os.Exit(1)
+		}
+
+		output.Write(data)
+		output.WriteString("\n---\n")
+	}
+
+	_, _ = fmt.Fprintf(os.Stdout, output.String())
+
+}
