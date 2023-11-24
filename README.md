e9f83ae422b5b2fd3a091a88eaea07a895347146
Nathan S. Martins <nathan.martins@argyle.com>
2023-11-24 16:47:18 -0300

    update version

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ files:
 
 ## Release Process
 
-Current version: `v2.0.5`
+Current version: `v2.0.6`
 
 To release a new version, install `goreleaser` and set your GitHub token:
 
--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ var rootCmd = &cobra.Command{
 
 		return errors.Wrap(err)
 	},
-	Version: "v2.0.5",
+	Version: "v2.0.6",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
